b46b2bc726f2994df0e61d6ee89e8fac6d1d98c3
Asad Mirza <asadbmirza97@gmail.com>
2024-05-28 14:19:48 -0400

    Finished the javascript logic behind the calculator
    Created new functions that handle the operations,
    changing button colours, and added event handlers
    to each button.

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function addButtons() {
             button.classList.add('num');
         }
         if (id[i] == "AC") {
-            button.style.backgroundColor = '#850a0a';
+            button.classList.add('AC');
         }
         else if (green_buttons.includes(id[i])) {
             button.classList.add('green_btn');
@@ -34,5 +34,157 @@ function addButtons() {
 }
 addButtons();
 
+function inputNumbers(e) {
+    if (clearDisplay || screen.textContent == '0') {
+        if (equalsPressed == true) {
+            inputOne = undefined;
+            storedNumber = undefined;
+        }
+        if (e.target.textContent == '.') {
+            screen.textContent = '0.';
+        }
+        else {
+            screen.textContent = e.target.textContent;
+        }
+        clearDisplay = false;
+    }
+    else if (screen.textContent.length < 15) {
+        if (!decimalPresent || e.target.textContent != ".") {
+            screen.textContent += e.target.textContent;
+        }
+        if (e.target.textContent == ".") {
+            decimalPresent = true;
+        }
+    }
+}
+
+function isPressed(e) {
+    if (e.target.classList.item(1) == 'AC') {
+        greenBtns.forEach(function (greenBtn) {
+            greenBtn.style.backgroundColor = "#3c6400";
+        });
+        nums.forEach(function (num) {
+            num.style.opacity = 1;
+        });
+        redBtn.style.backgroundColor = '#da1313';
+    }
+    if (e.target.classList.item(1) == 'green_btn') {
+        redBtn.style.opacity = "#850a0a";
+        greenBtns.forEach(function (greenBtn) {
+            greenBtn.style.backgroundColor = "#3c6400";
+        });
+        e.target.style.backgroundColor = "#78c701";
+        
+    }
+    else {
+        redBtn.style.backgroundColor = "#850a0a";
+        nums.forEach(function (num) {
+            num.style.opacity = 1;
+        });
+        e.target.style.opacity = "80%";
+    }
+}
+
+function operatorFunction(e) {
+    inputOne = screen.textContent;
+    equalsPressed = false;
+
+    if (e.target.textContent == '+/-') {
+        if (screen.textContent[0] == '-') {
+            screen.textContent = screen.textContent.substring(1);
+        }
+        else if (screen.textContent.length < 15) {
+            screen.textContent = "-" + screen.textContent;
+        }
+    }
+    else if (e.target.textContent == "%") {
+        if (storedNumber != undefined) {
+            screen.textContent = screen.textContent*0.01*storedNumber;
+        }
+    }
+    else {
+        if (storedNumber == undefined) {
+            storedNumber = inputOne;
+            inputOne = undefined; 
+            clearDisplay = true;
+            currentOperation = e.target.textContent;
+            if (e.target.textContent == '=') {
+                storedNumber = undefined;
+                equalsPressed = true;
+                currentOperation = undefined;
+            }
+        }
+        else {
+            storedNumber = operator(currentOperation, +storedNumber, +inputOne);
+            screen.textContent = (storedNumber + "").substring(0,15);
+            inputOne = undefined;
+            clearDisplay = true;
+            if (e.target.textContent == '=') {
+                equalsPressed = true;
+                currentOperation = undefined;
+                storedNumber = undefined;
+            }
+            else {
+                currentOperation = e.target.textContent;
+            }
+ 
+        }
+
+        //Colour the buttons
+    }   
+}
+
+function operator(sign, a, b) {
+    switch (sign) {
+        case '+':
+            return a + b;
+        case "-":
+            return a - b;
+        case '*':
+            return a * b;
+        case "/":
+            if (a == 0) {
+                return 10000000;
+            }
+            return a / b;
+    }
+}
+
+function clear(e) {
+    screen.textContent = '0';
+    clearDisplay = true;
+    inputOne = undefined;
+    storedNumber = undefined;
+    currentOperation = undefined;
+    equalsPressed = false;
+
+} 
+
+
+//Handles the input of numbers and decimals
+const nums = document.querySelectorAll(".num");
+let decimalPresent = false;
+nums.forEach(function (num) {
+    num.addEventListener('click', inputNumbers);
+    num.addEventListener('click', isPressed);
+});
+
+//Handles the input of operators
+//For handling display clears and keeping track of operations
+let clearDisplay = true;
+let inputOne = undefined;
+let storedNumber = undefined;
+let currentOperation = undefined;
+let equalsPressed = false;
+const greenBtns = document.querySelectorAll(".green_btn");
+greenBtns.forEach(function (greenBtn) {
+    greenBtn.addEventListener('click', operatorFunction);
+    greenBtn.addEventListener('click', isPressed);
+});
+
+//Handling AC
+const redBtn = document.querySelector(".AC");
+redBtn.addEventListener('click', clear);
+redBtn.addEventListener('click', isPressed);
 
 
